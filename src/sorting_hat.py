4f0550c832dd0c5513304144610503642dd415da
Niklas Weber <niklas.weber@booking.com>
2022-08-27 23:12:29 +0200

    first full version solving test case

--- src/sorting_hat.py
+++ src/sorting_hat.py
@@ -9,17 +9,17 @@ CourseMaxStudents: TypeAlias = Dict[str, int]
 
 def get_example_problem():
     student_preferences: StudentPreferences = {
-        'alice': ['alchemy', 'sorcery', 'arithmancy'],
-        'bob': ['alchemy', 'sorcery', 'magical beasts'],
-        'mike truck': ['alchemy', 'quidditch', 'sorcery'],
-        'bobson dugnutt': ['alchemy', 'quidditch', 'magical beasts']
+        "alice": ["alchemy", "sorcery", "arithmancy"],
+        "bob": ["alchemy", "sorcery", "magical beasts"],
+        "mike truck": ["alchemy", "quidditch", "sorcery"],
+        "bobson dugnutt": ["alchemy", "quidditch", "magical beasts"],
     }
     course_max_students: CourseMaxStudents = {
-        'alchemy': 2,
-        'sorcery': 1,
-        'arithmancy': 10,
-        'magical beasts': 30,
-        'quidditch': 30
+        "alchemy": 2,
+        "sorcery": 1,
+        "arithmancy": 10,
+        "magical beasts": 30,
+        "quidditch": 30,
     }
     return student_preferences, course_max_students
 
@@ -27,27 +27,55 @@ def get_example_problem():
 class CourseAssignmentVariables:
     def __init__(self, initial_variables: List[Tuple[str, str, cp_model.IntVar]]):
         self.variables: DataFrame = DataFrame(
-            data=initial_variables,
-            columns=['student', 'course', 'variable']
+            data=initial_variables, columns=["student", "course", "variable"]
         )
 
     def __eq__(self, other: Any):
         if not isinstance(other, self.__class__):
             return False
         # not sure how to meaningfully compare the IntVar objects, so dropping those for now
-        return self.variables[['student', 'course']].equals(other.variables[['student', 'course']])
+        return self.variables[["student", "course"]].equals(
+            other.variables[["student", "course"]]
+        )
 
     def get_by_student_name(self, name: str) -> List[cp_model.IntVar]:
-        student_vars: List[cp_model.IntVar] = self.variables.query(f"student == '{name}'")['variable'].to_list()
+        student_vars: List[cp_model.IntVar] = self.variables.query(
+            f"student == '{name}'"
+        )["variable"].to_list()
         return student_vars
 
+    def get_by_course_name(self, name: str) -> List[IntVar]:
+        course_vars: List[cp_model.IntVar] = self.variables.query(
+            f"course == '{name}'"
+        )["variable"].to_list()
+        return course_vars
 
-def generate_course_assignment_variables(students: StudentPreferences, courses: CourseMaxStudents, model: CpModel) -> CourseAssignmentVariables:
+    def get_by_student_name_and_courses(
+        self, student_name: str, course_names: List[str]
+    ):
+        variables: List[IntVar] = self.variables[
+            (self.variables["student"] == student_name)
+            & (self.variables["course"].isin(course_names))
+        ]["variable"].to_list()
+        return variables
+
+    def get_all(self) -> List[IntVar]:
+        return self.variables['variable'].to_list()
+
+
+def generate_course_assignment_variables(
+    students: StudentPreferences, courses: CourseMaxStudents, model: CpModel
+) -> CourseAssignmentVariables:
     student_names: List[str] = list(students.keys())
     course_names: List[str] = list(courses.keys())
     initial_variables: List[Tuple[str, str, cp_model.IntVar]] = [
-        (student_name, course_name, model.NewIntVar(0, 1, f'{student_name} in {course_name}'))
-        for student_name in student_names for course_name in course_names
+        (
+            student_name,
+            course_name,
+            model.NewIntVar(0, 1, f"{student_name} in {course_name}"),
+        )
+        for student_name in student_names
+        for course_name in course_names
     ]
     assignments = CourseAssignmentVariables(initial_variables)
     return assignments
@@ -59,53 +87,22 @@ def main():
     student_preferences, course_max_students = get_example_problem()
     model = cp_model.CpModel()
     assignment_variables: CourseAssignmentVariables = generate_course_assignment_variables(
-        student_preferences,
-        course_max_students,
-        model
+        student_preferences, course_max_students, model
     )
 
-    student_names: list = list(student_preferences.keys())
-    exactly_one_course_constraints: List[BoundedLinearExpression] = []
-    for student_name in student_names:
-        variables_for_student: List[cp_model.IntVar] = assignment_variables.get_by_student_name(student_name)
-        constraint: BoundedLinearExpression = sum(variables_for_student) == 1
-        exactly_one_course_constraints.append(constraint)
+    exactly_one_course_constraints = generate_constraints_exactly_one_course_per_student(assignment_variables, student_preferences)
+    max_students_per_course_constraints = generate_constraints_max_students_per_course(assignment_variables, course_max_students)
+    only_preferred_courses_constraints = generate_constraints_only_preferred_courses(assignment_variables, course_max_students, student_preferences)
 
-    # for c in constraints:
-    #     model.Add(c)
+    all_constraints: List[
+        BoundedLinearExpression
+    ] = exactly_one_course_constraints + max_students_per_course_constraints + only_preferred_courses_constraints
+    for constraint in all_constraints:
+        model.Add(constraint)
 
-
-    exactly_one_course_constraints = generate_each_student_exactly_one_course_constraints(
-        student_preferences,
-        course_max_students,
-        assignment_variables,
-        model
+    total_cost = generate_cost(
+        student_preferences, assignment_variables
     )
-    model.Add(*exactly_one_course_constraints)
-
-    no_more_than_max_students_in_course_constraints = generate_max_students_in_course_constraints(
-        student_preferences,
-        course_max_students,
-        assignment_variables,
-        model
-    )
-    model.Add(*no_more_than_max_students_in_course_constraints)
-
-    only_preferred_courses_constraints = generate_only_preferred_courses_constraints(
-        student_preferences,
-        course_max_students,
-        assignment_variables,
-        model
-    )
-    model.Add(*only_preferred_courses_constraints)
-
-    total_cost = make_total_cost_expression(
-        student_preferences,
-        course_max_students,
-        assignment_variables,
-        model
-    )
-
     model.Minimize(total_cost)
 
     # see for example: https://developers.google.com/optimization/cp/cp_example
@@ -113,9 +110,66 @@ def main():
     status = solver.Solve(model)
 
     if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
-        print(f'Maximum of objective function: {solver.ObjectiveValue()}\n')
-        print(f'x = {solver.Value(x)}')
-        print(f'y = {solver.Value(y)}')
-        print(f'z = {solver.Value(z)}')
+        print(f"Minimum of objective function: {solver.ObjectiveValue()}\n")
+        all_assignments: List[IntVar] = assignment_variables.get_all()
+        for assignment in all_assignments:
+            print(f'{assignment} = {solver.Value(assignment)}')
     else:
-        print('No solution found.')
+        print("No solution found.")
+
+
+def generate_constraints_only_preferred_courses(assignment_variables: CourseAssignmentVariables, course_max_students: CourseMaxStudents, student_preferences: StudentPreferences) -> List[BoundedLinearExpression]:
+    only_preferred_courses_constraints: List[BoundedLinearExpression] = []
+    all_course_name_set: set = set(course_max_students.keys())
+    student_names: List[str] = list(student_preferences.keys())
+    for student_name in student_names:
+        student_preferred_course_set: set = set(student_preferences[student_name])
+        non_preferred_courses: set = all_course_name_set - student_preferred_course_set
+        non_preferred_assign_vars: List[
+            IntVar
+        ] = assignment_variables.get_by_student_name_and_courses(
+            student_name, non_preferred_courses
+        )
+        for av in non_preferred_assign_vars:
+            only_preferred_courses_constraints.append(av == 0)
+    return only_preferred_courses_constraints
+
+
+def generate_constraints_max_students_per_course(assignment_variables: CourseAssignmentVariables, course_max_students: CourseMaxStudents) -> List[BoundedLinearExpression]:
+    course_names: list[str] = list(course_max_students.keys())
+    max_students_per_course_constraints: List[BoundedLinearExpression] = []
+    for course_name in course_names:
+        course_max_nr_students: int = course_max_students[course_name]
+        variables_for_course: List[IntVar] = assignment_variables.get_by_course_name(
+            course_name
+        )
+        constraint: BoundedLinearExpression = sum(
+            variables_for_course
+        ) <= course_max_nr_students
+        max_students_per_course_constraints.append(constraint)
+    return max_students_per_course_constraints
+
+
+def generate_constraints_exactly_one_course_per_student(assignment_variables: CourseAssignmentVariables, student_preferences: StudentPreferences) -> List[BoundedLinearExpression]:
+    student_names: list = list(student_preferences.keys())
+    exactly_one_course_constraints: List[BoundedLinearExpression] = []
+    for student_name in student_names:
+        variables_for_student: List[
+            cp_model.IntVar
+        ] = assignment_variables.get_by_student_name(student_name)
+        constraint: BoundedLinearExpression = sum(variables_for_student) == 1
+        exactly_one_course_constraints.append(constraint)
+    return exactly_one_course_constraints
+
+
+def generate_cost(students: StudentPreferences, course_assignments: CourseAssignmentVariables) -> BoundedLinearExpression:
+    cost_terms: List[BoundedLinearExpression] = []
+    for student_name, course_list in students.items():
+        # note that courses in list are ordered by preference
+        for preference_index, course in enumerate(course_list):
+            assign_var: IntVar = course_assignments.get_by_student_name_and_courses(student_name, [course])[0]
+            cost_terms.append(preference_index * assign_var)
+    cost: BoundedLinearExpression = sum(cost_terms)
+    return cost
+
+
--- test/test_sorting_hat.py
+++ test/test_sorting_hat.py
@@ -3,6 +3,7 @@ from typing import Dict, List
 import sorting_hat
 from sorting_hat import StudentPreferences, CourseMaxStudents, CourseAssignmentVariables
 from ortools.sat.python import cp_model
+from ortools.sat.python.cp_model import IntVar, BoundedLinearExpression
 
 
 def test_has_example_problem():
@@ -36,4 +37,53 @@ def test_creates_assignment_variables():
     assert course_assignments == expected_variables
 
 
-# def test_makes_
+def test_gets_cp_variables_by_course_name():
+    model: cp_model.CpModel = cp_model.CpModel()
+    assignment_data = [
+        ('alice', 'course_1', model.NewIntVar(0, 1, 'alice in course_1')),
+        ('alice', 'course_2', model.NewIntVar(0, 1, 'alice in course_2')),
+    ]
+    variables: CourseAssignmentVariables = CourseAssignmentVariables(
+        assignment_data
+    )
+    var_list_expected: List[IntVar] = [assignment_data[0][2]]
+    var_list_returned: List[IntVar] = variables.get_by_course_name('course_1')
+    assert var_list_returned == var_list_expected
+
+
+def test_makes_cost_expression():
+    students: StudentPreferences = {
+        'alice': ['course_1', 'course_2'],
+    }
+    courses: CourseMaxStudents = {
+        'course_1': 1,
+        'course_2': 1,
+        'course_3': 1,
+    }
+    model = cp_model.CpModel()
+    course_assignments: CourseAssignmentVariables = sorting_hat.generate_course_assignment_variables(
+        students,
+        courses,
+        model
+    )
+    alice_in_c1, alice_in_c2 = course_assignments.get_by_student_name_and_courses('alice', ['course_1', 'course_2'])
+    # note that 'course_3' does not even appear here. we make sure alice can never be assigned to a course she has not listed
+    # as a preference through other constraints
+    expected_cost: BoundedLinearExpression = 0 * alice_in_c1 + 1 * alice_in_c2
+    cost: BoundedLinearExpression = sorting_hat.generate_cost(students, course_assignments)
+    assert cost == expected_cost
+
+
+def test_gest_all_assignments():
+    model: cp_model.CpModel = cp_model.CpModel()
+    assignment_data = [
+        ('alice', 'course_1', model.NewIntVar(0, 1, 'alice in course_1')),
+        ('alice', 'course_2', model.NewIntVar(0, 1, 'alice in course_2')),
+    ]
+    assignments: CourseAssignmentVariables = CourseAssignmentVariables(
+        assignment_data
+    )
+    all_assignments_expected: List[IntVar] = [data[2] for data in assignment_data]
+    all_assignments: List[IntVar] = assignments.get_all()
+    assert all_assignments == all_assignments_expected
+

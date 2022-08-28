50aa7a0bddeec24ea31c767ddf30802a7426cbee
Niklas Weber <niklas.weber@booking.com>
2022-08-28 22:06:28 +0200

    documentation

--- src/sorting_hat.py
+++ src/sorting_hat.py
@@ -255,8 +255,10 @@ def solve_from_and_to_files(
 @click.argument('solution_file')
 def solve_from_command_line_args(capacity_file: str, student_file: str, solution_file: str) -> None:
     """
-    Read course capacities from CAPACITY_FILE (CSV format), read student preferences from STUDENT FILE,
-    attempt to solve optimally and write output to SOLUTION_FILE
+    Read course capacities from CAPACITY_FILE, read student preferences from STUDENT FILE,
+    attempt to solve optimally and write output to SOLUTION_FILE (will be created if it does not exist).
+
+    Input files should be CSV files. Output will be written as CSV as well.
     """
     cap_file: Path = Path(capacity_file)
     stud_file: Path = Path(student_file)
